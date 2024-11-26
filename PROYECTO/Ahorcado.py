a3878557456bba2f7b0676fdb572e6bd8bf17699
Giuls24-web <rogerfederer20015@gmail.com>
2024-11-25 22:38:37 -0500

    Cambio 1

---
+++ PROYECTO/Ahorcado.py
@@ -0,0 +1,12 @@
+print("EL AHORCADO")
+#Temática (Animales, Deportes, Objetos del hogar, Partes del computador, Lenguajes de Programación)
+Letra=input("Escribe una letra: ")
+i=0
+#Biblioteca de Animales (Perro, Gato, Gallina, Pato, Serpiente, Ganzo, Camello)
+#Biblioteca de Deportes (Perro, Gato, Gallina, Pato, Serpiente, Ganzo, Camello)
+#Biblioteca de Objetos del hogar (Cama, Cocina, Refrigerador, Sillón, Televisión)
+#Biblioteca de Partes del Computador (Pantalla, Mouse, Teclado, CPU, Parlantes, Impresora)
+#Biblioteca de Lenguajes de Programación (C++, Java, Python, Ruby, HTML)
+i=i+1
+print("TIENES SUERTE")
+print("MALA SUERTE")
