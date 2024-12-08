d3d744ee043eb15b7334b5c22a51b63448a90f90
Giuls24-web <rogerfederer20015@gmail.com>
2024-12-08 03:04:18 -0500

    Avance 2

--- PROYECTO/Ahorcado.py
+++ PROYECTO/Ahorcado.py
@@ -1,6 +1,6 @@
 print("EL AHORCADO")
 #Temática (Animales, Deportes, Lenguajes de Programación)
-Dibujo= [" | "," | ", "/", "O", " \ "," | ", "/", " \ " ]
+Dibujo= ["  |  ","  |  ", "  O  "," /|\ ", "  |  "," / \  "]
 i=0
 a=0
 print("Tenemos Diferentes Temáticas")
@@ -9,28 +9,36 @@ print("2. Deportes")
 print("3. Lenguaje de Programación")
 Palabra=int(input("Elije un número: "))
 
+#Condicional para la elección de temática
 if Palabra==1:
+ Palabra=int(input("Escribe un número del 1-7: "))
  Animales=("Perro", "Gato", "Gallina", "Pato", "Serpiente", "Ganzo", "Camello")
- Palabra=str("Perro")
+ Palabra=Animales[Palabra]
 elif Palabra==2:
+ Palabra=int(input("Escribe un número del 1-6: "))
  Deportes=("Futbol", "Basket", "Tennis","Badminton", "Volleyball", "Beisbol")
- Letra=str("Basket")
+ Palabra=Deportes[Palabra]
 else: 
+ Palabra=int(input("Escribe un número del 1-6: "))
  Lenguajes_de_Programación=("C++", "Java", "Python", "Ruby", "HTML")
- Palabra=str("Python")
+ Palabra=Lenguajes_de_Programación[Palabra]
 
 while True:  # Bucle infinito controlado por condiciones internas
     Letra = input("Escribe una letra: ")
-    if Letra == "p" or Letra == "e" or Letra == "r" or Letra == "o":
+    if Letra in Palabra:
         a += 1
         print(a)
         print(f"¡TIENES SUERTE! Tu letra fue: {Letra}")
         if a >= 5:  # Condición de victoria
-            print("¡GANASTE! Llegaste a 5 letras correctas.")
+            print(f"¡GANASTE! Tu pallabra es {Palabra}")
             break
     else:
         i += 1
-        print("MALA SUERTE!")
-        if i >= 8:  # Condición de derrota
-            print("¡PERDISTE! Llegaste a 8 intentos incorrectos.")
-            break
+        print("MALA SUERTE")
+        for j in range(i):
+          print(Dibujo[j])
+        if i >= 5:  # Condición de derrota
+             print(f"¡PERDISTE!.La palabra es: {Palabra}")
+             for j in range(6):
+              print(Dibujo[j])
+             break
