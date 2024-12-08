eb096ee3556859c3fad5c1dc6da771304fcf7b97
Giuls24-web <rogerfederer20015@gmail.com>
2024-12-08 01:57:27 -0500

    Actualización2

--- PROYECTO/Ahorcado.py
+++ PROYECTO/Ahorcado.py
@@ -1,12 +1,36 @@
 print("EL AHORCADO")
-#Temática (Animales, Deportes, Objetos del hogar, Partes del computador, Lenguajes de Programación)
-Letra=input("Escribe una letra: ")
+#Temática (Animales, Deportes, Lenguajes de Programación)
+Dibujo= [" | "," | ", "/", "O", " \ "," | ", "/", " \ " ]
 i=0
-#Biblioteca de Animales (Perro, Gato, Gallina, Pato, Serpiente, Ganzo, Camello)
-#Biblioteca de Deportes (Perro, Gato, Gallina, Pato, Serpiente, Ganzo, Camello)
-#Biblioteca de Objetos del hogar (Cama, Cocina, Refrigerador, Sillón, Televisión)
-#Biblioteca de Partes del Computador (Pantalla, Mouse, Teclado, CPU, Parlantes, Impresora)
-#Biblioteca de Lenguajes de Programación (C++, Java, Python, Ruby, HTML)
-i=i+1
-print("TIENES SUERTE")
-print("MALA SUERTE")
+a=0
+print("Tenemos Diferentes Temáticas")
+print("1. Animales")
+print("2. Deportes")
+print("3. Lenguaje de Programación")
+Palabra=int(input("Elije un número: "))
+
+if Palabra==1:
+ Animales=("Perro", "Gato", "Gallina", "Pato", "Serpiente", "Ganzo", "Camello")
+ Palabra=str("Perro")
+elif Palabra==2:
+ Deportes=("Futbol", "Basket", "Tennis","Badminton", "Volleyball", "Beisbol")
+ Letra=str("Basket")
+else: 
+ Lenguajes_de_Programación=("C++", "Java", "Python", "Ruby", "HTML")
+ Palabra=str("Python")
+
+while True:  # Bucle infinito controlado por condiciones internas
+    Letra = input("Escribe una letra: ")
+    if Letra == "p" or Letra == "e" or Letra == "r" or Letra == "o":
+        a += 1
+        print(a)
+        print(f"¡TIENES SUERTE! Tu letra fue: {Letra}")
+        if a >= 5:  # Condición de victoria
+            print("¡GANASTE! Llegaste a 5 letras correctas.")
+            break
+    else:
+        i += 1
+        print("MALA SUERTE!")
+        if i >= 8:  # Condición de derrota
+            print("¡PERDISTE! Llegaste a 8 intentos incorrectos.")
+            break
